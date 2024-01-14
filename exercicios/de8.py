8e235e2b7b45b96756d6cb97db2bf2ef8f427184
giovannamajory <giovannamajorysts@hotmail.com>
2024-01-14 00:08:47 -0300

    Update de8.py

--- exercicios/de8.py
+++ exercicios/de8.py
@@ -3,4 +3,4 @@ n1 = float( input('Digite seu número: '))
 me = n1/ 1000
 c = n1 * 100
 m = n1 * 1000
-print ('Você tem {} metros, centimentos {} e milimetros  {}' .format(me, c, m))
+print ('Você tem {} metros, centimentos {} e milimetros {}.' .format(me, c, m))
